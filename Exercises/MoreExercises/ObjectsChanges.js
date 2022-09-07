fb68ed6c57f83e84a94814bd0067e9bce3390bd6
DianaB <baditadianaelena@gmail.com>
2022-09-07 17:08:52 +0300

    Mai multe exercitii

---
+++ Exercises/MoreExercises/ObjectsChanges.js
@@ -0,0 +1,9 @@
+const car = {type: "Audi", model:"A4", color: "pink"};
+    animals = {breed:"dog", surname:"Izzy"}
+car.color = "red";
+car.owner = "Diana";
+animals.age ="4";
+console.log(car);
+console.log(animals);
+
+
---
+++ Exercises/MoreExercises/OperatorsExercise.js
@@ -0,0 +1,31 @@
+a=2;
+a+=1;
+
+console.log(a);
+
+b=3;
+c=4;
+
+d=c%b;
+
+console.log(d);
+
+let x = "Mama";
+let y = "si tata";
+let z = x + " " + y;
+console.log(z);
+
+var m=2;
+var n = 3;
+var m=7;
+
+var p = m-n;
+console.log(p);
+
+let ab = 5+5;
+let cd = "5"+5;
+let xy = "Ana" + " " + 2 + 3;
+
+console.log(ab);
+console.log(cd);
+console.log(xy);
