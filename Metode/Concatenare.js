552f2543971fb1c5565168f9a136166d6c1ac2b3
DianaB <baditadianaelena@gmail.com>
2022-09-08 17:18:36 +0300

    Inca 2 ex

---
+++ Metode/Concatenare.js
@@ -0,0 +1,6 @@
+
+let x = "Diana";
+let y = "Badita";
+
+let concatenare = x.concat(y);
+console.log(concatenare);
---
+++ Metode/ExCuArrays.js
@@ -0,0 +1,15 @@
+
+const masini = ["Audi", "BMW", "VOLVO"];
+let x=masini[2];
+console.log(x);
+
+const familie = {mama:"Cristina", tata:"Jonny", fiica:"Ina"};
+const z = familie.tata;
+console.log("Tatal este: ", z);
+
+
+
+
+const persoana = {gen: "femeie", nume: "Ana", varsta:"25", job:"inginer"};
+const a = persoana.job;
+console.log(a.toUpperCase());
